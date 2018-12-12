e758fff74bba88fb7bcd495b6c6896deeff6e58e
min <mjnorthwesternwork1121@gmail.com>
2018-12-11 22:44:54 -0600

    adding new files and changes for the app

---
+++ key.js
@@ -0,0 +1,11 @@
+//Greet the user
+console.log('This is loaded')
+
+//Get Spotify package
+exports.spotify = {
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
+  };
+
+
+
---
+++ logic.js
@@ -0,0 +1,10 @@
+//Greet the user
+console.log("Hello")
+
+
+// Api for...
+const options = {
+    provider: "mapquest",
+    apiKey: "YOUR-MAPQUEST-API-CONSUMER-KEY"
+  };
+  
