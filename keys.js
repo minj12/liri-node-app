41264a41b035e1100db2e2292ab47e5e3cd7a15b
min <mjnorthwesternwork1121@gmail.com>
2018-12-18 02:56:11 -0600

    Added new code. Add lets and vars, and fix spotify vug

--- keys.js
+++ keys.js
@@ -1,7 +1,7 @@
 //log this is loaded
 console.log('This is loaded')
 
-//Get Spotify package
+//Get Spotify 'id' and "secret keys" 
 exports.spotify = {
     id: process.env.SPOTIFY_ID,
     secret: process.env.SPOTIFY_SECRET
